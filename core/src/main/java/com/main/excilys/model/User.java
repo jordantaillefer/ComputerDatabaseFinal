07b760e6abc8464ef0bf4ce1c8b92d7148dda559
MatthieuLemaile <matlem35@yahoo.fr>
2017-05-15 11:23:00 +0200

    include UserRole in User and vice-versa

--- core/src/main/java/com/main/excilys/model/User.java
+++ core/src/main/java/com/main/excilys/model/User.java
@@ -1,9 +1,12 @@
 package com.main.excilys.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Pattern;
 
@@ -11,86 +14,106 @@ import javax.validation.constraints.Pattern;
 @Table(name = "users")
 public class User implements Serializable {
 
-  /**
-   *
-   */
-  private static final long serialVersionUID = -3882079512414316136L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -3882079512414316136L;
 
-  @Id
-  @Pattern(regexp = "^[a-zA-Z0-9 ._-]+$", message = "The name need at least one char or number")
-  private String username;
-  private String password;
-  private boolean enabled;
+    @Id
+    @Pattern(regexp = "^[a-zA-Z0-9 ._-]+$", message = "The name need at least one char or number")
+    private String            username;
+    private String            password;
+    private boolean           enabled;
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    private List<UserRole>    userRoles;
 
-  public User(String username, String password, boolean enabled) {
-    this.username = username;
-    this.password = password;
-    this.enabled = enabled;
-  }
+    public User(String username, String password, boolean enabled) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+    }
 
-  public User() {
-  }
-  public String getUsername() {
-    return username;
-  }
-  public void setUsername(String username) {
-    this.username = username;
-  }
-  public String getPassword() {
-    return password;
-  }
-  public void setPassword(String password) {
-    this.password = password;
-  }
-  public boolean isEnabled() {
-    return enabled;
-  }
-  public void setEnabled(boolean enabled) {
-    this.enabled = enabled;
-  }
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + (enabled ? 1231 : 1237);
-    result = prime * result + (password == null ? 0 : password.hashCode());
-    result = prime * result + (username == null ? 0 : username.hashCode());
-    return result;
-  }
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
+    public User() {
     }
-    if (obj == null) {
-      return false;
+
+    public String getUsername() {
+        return username;
     }
-    if (this.getClass() != obj.getClass()) {
-      return false;
+
+    public void setUsername(String username) {
+        this.username = username;
     }
-    User other = (User) obj;
-    if (enabled != other.enabled) {
-      return false;
+
+    public String getPassword() {
+        return password;
     }
-    if (password == null) {
-      if (other.password != null) {
-        return false;
-      }
-    } else if (!password.equals(other.password)) {
-      return false;
+
+    public void setPassword(String password) {
+        this.password = password;
     }
-    if (username == null) {
-      if (other.username != null) {
-        return false;
-      }
-    } else if (!username.equals(other.username)) {
-      return false;
+
+    public boolean isEnabled() {
+        return enabled;
     }
-    return true;
-  }
-  @Override
-  public String toString() {
-    return "User [username=" + username + ", password=" + password + ", enabled=" + enabled + "]";
-  }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(List<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (enabled ? 1231 : 1237);
+        result = prime * result + (password == null ? 0 : password.hashCode());
+        result = prime * result + (username == null ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        User other = (User) obj;
+        if (enabled != other.enabled) {
+            return false;
+        }
+        if (password == null) {
+            if (other.password != null) {
+                return false;
+            }
+        } else if (!password.equals(other.password)) {
+            return false;
+        }
+        if (username == null) {
+            if (other.username != null) {
+                return false;
+            }
+        } else if (!username.equals(other.username)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User [username=" + username + ", password=" + password + ", enabled=" + enabled
+                + "]";
+    }
 
 }
--- service/src/main/java/com/main/excilys/mapper/UserToDtoMapper.java
+++ service/src/main/java/com/main/excilys/mapper/UserToDtoMapper.java
@@ -1,9 +1,6 @@
 package com.main.excilys.mapper;
 
-import java.util.List;
-
 import com.main.excilys.model.User;
-import com.main.excilys.model.UserRole;
 import com.main.excilys.model.dto.UserDto;
 
 public class UserToDtoMapper {
@@ -11,9 +8,9 @@ public class UserToDtoMapper {
     return new User(userDto.getUsername(), userDto.getPassword(), userDto.isEnabled());
   }
 
-  public static UserDto toUserDto(User user, List<UserRole> userRoles) {
+    public static UserDto toUserDto(User user) {
       StringBuilder sb = new StringBuilder();
-        userRoles.forEach(roles -> sb.append(roles.getRole() + ", "));
+        user.getUserRoles().forEach(roles -> sb.append(roles.getRole() + ", "));
         return new UserDto(user.getUsername(), user.getPassword(), user.isEnabled(),
                 sb.substring(0, sb.length() - 2));
   }
