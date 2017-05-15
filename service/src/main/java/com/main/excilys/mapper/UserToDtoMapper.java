0af283b3379796a091bc44da0d112a3e0598a632
MatthieuLemaile <matlem35@yahoo.fr>
2017-05-15 16:04:08 +0200

    Dashboard correctly display number of users, Doesn't display ROLE_USER anymore

--- service/src/main/java/com/main/excilys/mapper/UserToDtoMapper.java
+++ service/src/main/java/com/main/excilys/mapper/UserToDtoMapper.java
@@ -10,11 +10,18 @@ import com.main.excilys.model.dto.UserDto;
 public class UserToDtoMapper {
     public static User toUser(UserDto userDto) {
         List<UserRole> userRoles = new ArrayList<>();
+        boolean userRolePresent = false;
         if (userDto.getRole() != null) {
             String roles[] = userDto.getRole().split(", ");
             for (String role : roles) {
                 UserRole userRole = new UserRole(userDto.getUsername(), role);
+                if (userRole.getRole().equals("ROLE_USER")) {
+                    userRolePresent = true;
+                }
                 userRoles.add(userRole);
+            }
+            if (!userRolePresent) {
+                userRoles.add(new UserRole(userDto.getUsername(), "ROLE_USER"));
             }
         }
         User user = new User(userDto.getUsername(), userDto.getPassword(), userDto.isEnabled());
@@ -24,9 +31,12 @@ public class UserToDtoMapper {
 
     public static UserDto toUserDto(User user) {
         StringBuilder sb = new StringBuilder();
-        user.getUserRoles().forEach(roles -> sb.append(roles.getRole() + ", "));
-        String roles = sb.length() > 2 ? sb.substring(0, sb.length() - 2) : "";
-        return new UserDto(user.getUsername(), user.getPassword(), user.isEnabled(), roles);
+        user.getUserRoles().forEach(roles -> {
+            if (roles.getRole().equals("ROLE_ADMIN")) {
+                sb.append("ROLE_ADMIN");
+            }
+        });
+        return new UserDto(user.getUsername(), user.getPassword(), user.isEnabled(), sb.toString());
     }
 
 }
